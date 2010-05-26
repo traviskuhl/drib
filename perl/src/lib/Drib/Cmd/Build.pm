55ecb38b2271ff928273c7f15203ed336cd080db
Travis <traviskuhl@yahoo.com>
2010-05-26 06:56:40 +0000

    starting

---
+++ perl/src/lib/Drib/Cmd/Build.pm
@@ -0,0 +1,12 @@
+#
+#  Drib::Cmd::Build
+# =================================
+#  (c) Copyright Travis Kuhl 2009-10
+#  
+#
+# This is free software. You may redistribute copies of it under the terms of
+# the GNU General Public License <http://www.gnu.org/licenses/gpl.html>.
+# There is NO WARRANTY, to the extent permitted by law.
+#
+
+package Drib::Cmd::Build;
