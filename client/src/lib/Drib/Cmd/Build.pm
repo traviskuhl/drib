740c19a99ddcab61cc24f8e3c1a48455998c8705
Travis <traviskuhl@yahoo.com>
2010-07-25 18:38:49 -0700

    added stub build module. added configure script.

--- client/src/lib/Drib/Cmd/Build.pm
+++ client/src/lib/Drib/Cmd/Build.pm
@@ -9,4 +9,55 @@
 # There is NO WARRANTY, to the extent permitted by law.
 #
 
+# build
 package Drib::Cmd::Build;
+
+# version
+our $VERSION => "1.0";
+
+# drib
+use Drib::Utils;
+
+# new 
+sub new {
+
+	# get 
+	my($ref, $drib) = @_;
+	
+	# self
+	my $self = {
+		
+		# drib 
+		'drib' => $drib,
+		
+		# shortcuts
+		'tmp' => $drib->{tmp},
+		
+		# commands
+		'commands' => [
+			{ 
+				'name' => 'build',
+				'help' => '', 
+				'alias' => ['bu','b'],
+				'options' => [
+				]
+			}
+		]
+		
+	};
+
+	# bless and return me
+	bless($self); return $self;
+
+}
+
+##
+## @brief run the create 
+##
+sub run {
+	
+	# get some stuff
+	my ($self, $cmd, $opts) = @_;
+	
+	
+}
